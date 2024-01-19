f4f15127c06b713dcc4c9c01052f3acf6fdd2b3a
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2024-01-19 23:26:22 +0100

    Add and revise Elsa Studio setup guide
    The Elsa Studio setup guide has been updated for both ASP.NET Blazor WebAssembly and ASP.NET Blazor Server. Modifications were made to elsa-studio JavaScript files, Program.cs files and Elsa-Studio.topic files. All changes aim to create a clearer and more comprehensive tutorial on how to set up Elsa Studio using both methods.

---
+++ writerside/v3/Writerside/snippets/elsa-studio/server/Program.cs
@@ -0,0 +1,59 @@
+using Elsa.Studio.Core.BlazorServer.Extensions;
+using Elsa.Studio.Dashboard.Extensions;
+using Elsa.Studio.Extensions;
+using Elsa.Studio.Login.BlazorServer.Extensions;
+using Elsa.Studio.Login.HttpMessageHandlers;
+using Elsa.Studio.Shell.Extensions;
+using Elsa.Studio.Workflows.Extensions;
+using Elsa.Studio.Workflows.Designer.Extensions;
+
+// Build the host.
+var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+
+// Register Razor services.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor(options =>
+{
+    // Register the root components.
+    options.RootComponents.RegisterCustomElsaStudioElements();
+});
+
+// Register shell services and modules.
+builder.Services.AddCore();
+builder.Services.AddShell(options => configuration.GetSection("Shell").Bind(options));
+builder.Services.AddRemoteBackend(
+    elsaClient => elsaClient.AuthenticationHandler = typeof(AuthenticatingApiHttpMessageHandler),
+    options => configuration.GetSection("Backend").Bind(options));
+builder.Services.AddLoginModule();
+builder.Services.AddDashboardModule();
+builder.Services.AddWorkflowsModule();
+
+// Configure SignalR.
+builder.Services.AddSignalR(options =>
+{
+    // Set MaximumReceiveMessageSize to handle large workflows.
+    options.MaximumReceiveMessageSize = 5 * 1024 * 1000; // 5MB
+});
+
+// Build the application.
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseResponseCompression();
+
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+// Run the application.
+app.Run();
