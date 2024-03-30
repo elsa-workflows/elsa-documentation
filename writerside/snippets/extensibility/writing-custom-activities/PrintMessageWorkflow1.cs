62328ed354a945855ea372510d5e0e53c10131e7
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2024-03-30 22:17:54 +0100

    Incremental work

---
+++ writerside/snippets/extensibility/writing-custom-activities/PrintMessageWorkflow1.cs
@@ -0,0 +1,15 @@
+using Elsa.Workflows;
+using Elsa.Workflows.Contracts;
+
+public class PrintMessageWorkflow : WorkflowBase
+{
+    protected override void Build(IWorkflowBuilder builder)
+    {
+        var message = builder.WithVariable<string>("Message", "Hello, World!");
+
+        builder.Root = new PrintMessage
+        {
+            Message = new(message)
+        };
+    }
+}
