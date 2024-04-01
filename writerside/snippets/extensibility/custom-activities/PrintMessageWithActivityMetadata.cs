d443bcea9f4c2e8110e881b0fa35f584293808b4
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2024-04-01 21:56:14 +0200

    Update activity description in PrintMessage class
    The activity description for the PrintMessage class has been updated. Previously, the description was 'Print Message', it has now been changed to 'Print a message to the console' to provide a more explicit understanding of the function's purpose.

--- writerside/snippets/extensibility/custom-activities/PrintMessageWithActivityMetadata.cs
+++ writerside/snippets/extensibility/custom-activities/PrintMessageWithActivityMetadata.cs
@@ -1,7 +1,7 @@
 using Elsa.Workflows;
 using Elsa.Workflows.Attributes;
 
-[Activity("MyCompany", "Print Message")]
+[Activity("MyCompany", "Print a message to the console")]
 public class PrintMessage : CodeActivity
 {
     protected override void Execute(ActivityExecutionContext context)
